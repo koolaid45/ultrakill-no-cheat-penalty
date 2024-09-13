de3677da651fde706a8447dd93915908d032a87a
yesman <80596641+koolaid45@users.noreply.github.com>
2024-09-12 21:49:47 -0700

    augh

---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+[HarmonyPatch]
+public class RemoveCheatConsequence
+{
+    
+[HarmonyPatch]
+    public class FunniRankPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GameStateManager), "CanSubmitScores", MethodType.Getter)]
+        static void ScoresSubmission(ref bool __result) => __result = true;
+    }
+}
---
+++ src/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace none_of_the_cheating_hazards
+{
+    public class Class1
+    {
+    }
+}
